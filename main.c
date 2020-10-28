bf844c0981805ea92fd7eb2298c53e0d47eff4a2
Dmitry <feddiman02@gmail.com>
2020-10-29 00:14:58 +0300

    Добавил вывод массива с помощью цикла while

--- main.c
+++ main.c
@@ -3,8 +3,14 @@
 int main(){
     int arr[10];
 
-    for(int i = 0; i<10; i++)
+    for(int i = 0; i < 10; i++)
         arr[i] = 1;
+
+    int i = 0;
+    while(i < 10){
+        printf("%d", arr[i]);
+        i++;
+    }
 
     return 0;
 }
